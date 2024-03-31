6739fb999df5df05ce3820f63db891f0cacaede2
ckcool6 <rzdtjb@gmail.com>
2024-04-01 01:49:02 +0800

    Changes

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
 import "@/app/blog"
 import Blog from "@/app/blog";
+import Footer from "@/app/foot";
 export default function Home(){
     return (
         <>
             <Blog></Blog>
+            <Footer></Footer>
         </>
     );
 }
